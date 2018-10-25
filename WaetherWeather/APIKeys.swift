2402795f05d204970cffc4743ab7eedd80514ff5
Kaleb Allen <allen.henry.kaleb@.com>
2018-10-25 12:39:37 -0500

    APIKey Files

---
+++ WaetherWeather/APIKeys.swift
@@ -0,0 +1,15 @@
+//
+//  APIKeys.swift
+//  WaetherWeather
+//
+//  Created by Kaleb Allen on 10/25/18.
+//  Copyright © 2018 Kaleb Allen. All rights reserved.
+//
+
+import Foundation
+
+//Struct to contain our API keys from DarkSky the Google Geocoding API
+struct APIKeys {
+    let darkSkyKey = ""
+    let googleKey = ""
+}
