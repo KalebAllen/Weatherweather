4d7b9eda7779fe280e2e8b431a7af0ece5a7d54f
Kaleb Allen <allen.henry.kaleb@.com>
2018-10-26 14:34:32 -0500

    Added a lot of case code

---
+++ WaetherWeather/DataModles/GeoCoding.swift
@@ -0,0 +1,12 @@
+//
+//  GeoCoding.swift
+//  WaetherWeather
+//
+//  Created by Kaleb Allen on 10/26/18.
+//  Copyright © 2018 Kaleb Allen. All rights reserved.
+//
+
+import Foundation
+class GeoCoding {
+    
+}
